308bd99940093be7eda9eaa29e4957b7d31728e6
Gabriel Domingues Volpi <gabriel.volpi@ifood.com.br>
2021-08-22 19:10:17 -0300

    creating kotlin from json class received from online API

---
+++ app/src/main/java/com/example/movie_app/data/vo/MovieDetails.kt
@@ -0,0 +1,23 @@
+package com.example.movie_app.data.vo
+
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieDetails(
+    val homepage: String,
+    val id: Int,
+    val overview: String,
+    val popularity: Double,
+    @SerializedName("poster_path")
+    val posterPath: String,
+    @SerializedName("release_date")
+    val releaseDate: String,
+    val revenue: Int,
+    val runtime: Int,
+    val status: String,
+    val tagline: String,
+    val title: String,
+    val video: Boolean,
+    @SerializedName("vote_average")
+    val rating: Double,
+)
